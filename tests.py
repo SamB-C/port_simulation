6e0f07cf87082c9d3acb75dc4a16024acf31c8d5
SamB-C <samson_bc@icloud.com>
2023-03-03 17:04:00 +0000

    Change empty containers to have None instead of ''

--- tests.py
+++ tests.py
@@ -6,24 +6,24 @@ class TestContainerFactory(unittest.TestCase):
     @unittest.expectedFailure
     def test_deafult_size_expressions(self):
         with self.assertRaises(Exception):
-            create_container('')
+            create_container(None)
 
     @unittest.expectedFailure
     def test_short_size_expressions(self):
         with self.assertRaises(Exception):
-            create_container('', 'short')
+            create_container(None, 'short')
 
     def test_contents(self):
-        container = create_container('')
-        self.assertEqual(container.contents, '')
+        container = create_container(None)
+        self.assertEqual(container.contents, None)
 
     def test_invalid_size_expression(self):
         with self.assertRaises(InvalidContainerSizeExpression):
-            create_container('', 'hi')
+            create_container(None, 'hi')
 
     def test_erroneous_size_expression(self):
         with self.assertRaises(TypeError):
-            create_container('', True)
+            create_container(None, True)
 
 
 class TestContainer(unittest.TestCase):
@@ -31,12 +31,12 @@ class TestContainer(unittest.TestCase):
     @unittest.expectedFailure
     def test_size_two_point_five_nine(self):
         with self.assertRaises(InvalidSizeException):
-            Container(2.59, '')
+            Container(2.59, None)
 
     def test_contents(self):
         with self.subTest():
-            container = Container(2.59, '')
-            expected = ''
+            container = Container(2.59, 'lion')
+            expected = 'lion'
             actual = container.contents
             self.assertEqual(expected, actual)
         with self.subTest():
@@ -48,31 +48,31 @@ class TestContainer(unittest.TestCase):
     @unittest.expectedFailure
     def test_size_six_point_zero_six(self):
         with self.assertRaises(InvalidSizeException):
-            Container(6.06, '')
+            Container(6.06, None)
 
     def test_size_invalid(self):
         with self.assertRaises(InvalidSizeException):
-            Container(1, '')
+            Container(1, None)
 
     def test_size_as_string(self):
         with self.assertRaises(TypeError):
-            Container('1', '')
+            Container('1', None)
 
     def test_size_as_boolean(self):
         with self.assertRaises(TypeError):
-            Container(True, '')
+            Container(True, None)
 
     @unittest.expectedFailure
     def test_size_as_int(self):
         with self.assertRaises(TypeError):
-            Container(10, '')
+            Container(10, None)
 
     def test_get_is_short(self):
-        container = create_container('', 'short')
+        container = Container(2.59, None)
         self.assertTrue(container.is_short)
 
     def test_get_is_short_false(self):
-        container = create_container('')
+        container = Container(6.06, None)
         self.assertFalse(container.is_short)
 
 
@@ -92,12 +92,12 @@ class TestContainerStack(unittest.TestCase):
         with self.assertRaises(ConatinerSizeConflict):
             containerStack = ContainerStack()
             containerStack.add_conatiner(create_container(None, 'short'))
-            container = Container(6.06, '')
+            container = Container(6.06, None)
             containerStack.add_conatiner(container)
 
     def test_add_valid_container(self):
         containerStack = ContainerStack()
-        container = Container(2.59, '')
+        container = Container(2.59, None)
         containerStack.add_conatiner(container)
         self.assertListEqual(containerStack.layers, [[None], [container]])
 
